7b148d081ac9c2bbae6a47cab1c8ff855d0700bb
Pirakaa <p.alistratov2@gmail.com>
2025-11-08 12:27:33 +0100

    removed test.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from stats import word_counter, character_counter, sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath, encoding="utf-8") as f:
         return f.read()
+
+if len(sys.argv) != 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
     
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     word_counter(text)
     characters = character_counter(text)
     characters_sorted = sort_dict(characters)
